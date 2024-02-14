ef894544a54a968bdcc0c74ff7d40532beb07162
unknown <williamsartijose@hotmail.com>
2024-02-14 09:38:40 +0000

    formData Models, Services

--- src/components/CardFilter/index.tsx
+++ src/components/CardFilter/index.tsx
@@ -1,16 +1,52 @@
+import { useState } from "react";
 import "./styles.css";
 
-export default function CardFilter() {
-  return (
-    <div className="dsf-cardFilter-container">
-      <div className="dsf-input-content">
-        <input type="text" placeholder="Preço mínimo" />
-      </div>
+type FomData = {
+  min?: number;
+  max?: number;
+};
 
-      <div className="dsf-input-content">
-        <input type="text" placeholder="Preço máximo" />
+export default function CardFilter() {
+  const [formData, setFormData] = useState<FomData>({});
+
+  function handleInputChange(event: any) {
+    const value = event.target.value;
+    const name = event.target.name;
+    setFormData({ ...formData, [name]: value });
+  }
+
+  function handleSubmit(event : any) {
+    event.preventDefault();
+    console.log(formData.min || 0);
+    console.log(formData.max || Number.MAX_VALUE);
+    
+    
+  }
+
+  return (
+    <form>
+      <div className="dsf-cardFilter-container">
+        <div className="dsf-input-content">
+          <input
+            name="min"
+            value={formData.min || ""}
+            type="text"
+            placeholder="Preço mínimo"
+            onChange={handleInputChange}
+          />
+        </div>
+
+        <div className="dsf-input-content">
+          <input
+            name="max"
+            value={formData.max || ""}
+            type="text"
+            placeholder="Preço máximo"
+            onChange={handleInputChange}
+          />
+        </div>
+        <button onClick={handleSubmit} type="submit" >Filtrar</button>
       </div>
-      <button>Filtrar</button>
-    </div>
+    </form>
   );
 }
